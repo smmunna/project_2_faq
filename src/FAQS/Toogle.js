603c3ab07b0b61b39b9b466a3444fdf324364d9f
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2022-10-28 23:15:20 -0700

    update toogle.js

--- src/FAQS/Toogle.js
+++ src/FAQS/Toogle.js
@@ -2,12 +2,21 @@ import React,{useState} from 'react'
 
 function Toogle(props) {
     const {id,ques,ans} = props;
+    const [show,setShow] = useState(false);
   return (
     <div>
         <div className="card">
             <h4>{id}. {ques}</h4>
-            <p>{ans}</p>
-            <button>+</button>
+            {
+                show ? <p>{ans}</p> : null
+            }
+            <div className="btn">
+                <button onClick={()=>{setShow(!show)}}>
+                    {
+                        show ? "-" : "+"
+                    }
+                </button>
+            </div>
         </div>
     </div>
   )
