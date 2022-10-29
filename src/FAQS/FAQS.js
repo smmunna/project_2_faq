74ba07a145398f572e23b1de24262b69fd50bb32
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2022-10-29 00:01:22 -0700

    Completed Toggle App

--- src/FAQS/FAQS.js
+++ src/FAQS/FAQS.js
@@ -13,5 +13,10 @@ export const data = [
         id:"3",
         desc:"What is the difference between a full stack developer and a back end developer?",
         ans:"A full stack developer is someone who can work on both the front end and the back end of a website. A back end developer is someone who can only work on the back end of a website."
+    },
+    {
+        id:"4",
+        desc:"What is the difference between a front end developer and a full stack developer?",
+        ans:"A front end developer is someone who can only work on the front end of a website. A full stack developer is someone who can work on both the front end and the back end of a website."
     }
 ]
--- src/FAQS/Toogle.js
+++ src/FAQS/Toogle.js
@@ -1,25 +1,27 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
 function Toogle(props) {
-    const {id,ques,ans} = props;
-    const [show,setShow] = useState(false);
+  const { id, ques, ans } = props;
+  const [show, setShow] = useState(false);
   return (
     <div>
-        <div className="card">
-            <h4>{id}. {ques}</h4>
-            {
-                show ? <p>{ans}</p> : null
-            }
-            <div className="btn">
-                <button onClick={()=>{setShow(!show)}}>
-                    {
-                        show ? "-" : "+"
-                    }
-                </button>
-            </div>
+      <div className="card">
+        <h4>
+          {id}. {ques}
+        </h4>
+        <div className="btn">
+          <button
+            onClick={() => {
+              setShow(!show);
+            }}
+          >
+            {show ? "-" : "+"}
+          </button>
         </div>
+        {show ? <p>{ans}</p> : null}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Toogle
+export default Toogle;
